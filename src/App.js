684609764d4fbb7d068c366c61de45e71f7905f8
Seannio <Sean_E_M@icloud.com>
2019-07-10 12:47:02 -0300

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Info from "./components/Info/Info";
 import Location from "./components/Location/Location";
 
 const App = () => {
   let [tempSymbol, setTempSymbol] = useState("F");
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    (async function fetchData() {
+      const response = await fetch(
+        "http://api.openweathermap.org/data/2.5/group?id=5368361,524901&units=imperial&appid=1139b4360eac6349eafa7d15da934d5c"
+      );
+      const data = await response.json();
+      setCities(data.list);
+    })();
+  }, []);
+
   return (
     <main>
       <Info
         tempSymbol={tempSymbol}
         setTempSymbol={() => setTempSymbol(tempSymbol === "F" ? "C" : "F")}
       />
-      <Location />
-      <Location />
+      <Location cities={cities[0]} tempSymbol={tempSymbol} />
+      <Location cities={cities[1]} tempsymbol={tempSymbol} />
     </main>
   );
 };
--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -5,26 +5,49 @@ import snow from "../../img/snow-icon.svg";
 import ca from "../../img/pic-1.jpg";
 import ru from "../../img/pic-2.jpg";
 
-const Location = () => {
+const Location = props => {
+  let currentTemp = 0;
+  if (props.cities) {
+    let fahrenheit = parseInt(props.cities.main.temp);
+    let celsius = parseInt((fahrenheit - 32) * (5 / 9));
+    if (props.tempSymbol === "C") {
+      currentTemp = celsius;
+    } else {
+      currentTemp = fahrenheit;
+    }
+  }
+
   return (
     <section>
       <div className="floater">
         <div className="icon-temp">
-          <img alt="sunicon" src={sun} />
-          <p id="temp">90</p>
+          {props.cities && props.cities.name === "Los Angeles" ? (
+            <img alt="sunicon" src={sun} />
+          ) : (
+            <img alt="sunicon" src={snow} />
+          )}
+          <p id="temp">{props.cities ? currentTemp : ""}°</p>
         </div>
-        <p>Sunny</p>
+        <p>{props.cities ? props.cities.weather[0].main.toUpperCase() : ""}</p>
       </div>
-      <img src={ca} alt="location" id="location-img" />
+      {props.cities && props.cities.name === "Los Angeles" ? (
+        <img src={ca} alt="LA" id="location-img" />
+      ) : (
+        <img src={ru} alt="RU" id="location-img" />
+      )}
+
       <div className="location">
-        <div>
-          <p>Los Angeles</p>
-          <p>CALIFORNIA</p>
-        </div>
-        <div>
-          <p>Moscow</p>
-          <p>Russia</p>
-        </div>
+        {props.cities && props.cities.name === "Los Angeles" ? (
+          <div>
+            <p>Los Angeles</p>
+            <p>CALIFORNIA</p>
+          </div>
+        ) : (
+          <div>
+            <p>Moscow</p>
+            <p>Russia</p>
+          </div>
+        )}
       </div>
     </section>
   );
